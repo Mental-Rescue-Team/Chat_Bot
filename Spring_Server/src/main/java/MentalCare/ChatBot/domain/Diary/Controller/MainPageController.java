3d95b29b12b0112150941079acd7cc0117c857ad
passionryu <rsy1225@naver.com>
2024-10-03 02:34:24 +0900

    feat :  메인 페이지 로드 시 호출될 api

---
+++ Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Controller/MainPageController.java
@@ -0,0 +1,6 @@
+package MentalCare.ChatBot.domain.Diary.Controller;
+
+public class MainPageController {
+    // TODO : 오늘의 날씨(이름) 불러오기 API 개발 -> 날씨 사진과 격려 메시지는 날씨 이름을 바탕으로 클라이언트 측에서 로드할 것임
+    // TODO : 이번달의 날씨 이모티콘 전부 불러오기 API 개발
+}
