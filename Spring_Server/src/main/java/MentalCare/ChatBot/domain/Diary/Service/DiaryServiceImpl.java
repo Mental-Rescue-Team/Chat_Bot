f0826ef2f46b59778054e467c1058a6713007ebc
passionryu <rsy1225@naver.com>
2024-10-10 16:41:19 +0900

    refactor : diary 감정과 날씨 매핑 서비스를 컨트롤러-서비스 분리 리펠토링

--- Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Service/DiaryServiceImpl.java
+++ Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Service/DiaryServiceImpl.java
@@ -4,23 +4,26 @@ import MentalCare.ChatBot.domain.Diary.DTO.Request.DiaryRequest;
 import MentalCare.ChatBot.domain.Diary.Entity.Diary;
 import MentalCare.ChatBot.domain.Diary.Repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryServiceImpl implements DiaryService {
 
     private final DiaryRepository diaryRepository;
+    private final OpenAiChatModel openAiChatModel;
 
-    /*요약하고 fast-api 로 데이터를 넘겨야 한다*/
-    //DB에 저장할 필요는 없음
     @Override
     public String SummarizeDiary(DiaryRequest diaryRequest) {
-        return "";
+        /*DTO.toStirng()후 AI연산 시행 결과값을 String type으로 return*/
+        return openAiChatModel.call(diaryRequest.toString());
     }
 
+    // TODO : 4컷 만화 생성 후 어떻게 저장하고 전송할지 확인
     @Override
     public String DrawComic(DiaryRequest diaryRequest) {
         return "";
@@ -28,12 +31,30 @@ public class DiaryServiceImpl implements DiaryService {
 
     @Override
     public String SaveDiary(DiaryRequest diaryRequest) {
-        return "";
+        /*실제 저장은 하지 않고, String type으로 변환후 return*/
+        return diaryRequest.toString();
     }
 
     @Override
     public String ClassifyEmotion(DiaryRequest diaryRequest) {
         return "";
+    }
+
+    @Override
+    public Map<String, String> WeatherMatch(String diaryEmotion) {
+
+        String weather;
+        String weatherEmoji;
+
+        switch (diaryEmotion) {
+            case "기쁨" -> {weather = "Sunny";weatherEmoji = "☀️";}
+            case "슬픔" -> {weather = "Rainy";weatherEmoji = "🌧️";}
+            case "분노" -> {weather = "Stormy";weatherEmoji = "🌩️";}
+            case "평온" -> {weather = "Cloudy";weatherEmoji = "☁️";}
+            case "불안" -> {weather = "Windy";weatherEmoji = "🌬️";}
+            default -> {weather = "Unknown";weatherEmoji = "❓";}
+        }
+        return Map.of(weather,weatherEmoji);
     }
 
     @Override
