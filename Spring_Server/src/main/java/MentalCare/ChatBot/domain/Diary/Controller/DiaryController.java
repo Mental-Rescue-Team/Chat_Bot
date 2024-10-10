1e9bddf1b282eb5199c9838655274c8b3de5f043
passionryu <rsy1225@naver.com>
2024-10-10 21:31:49 +0900

    feat : 일기 요약 테스팅 + 만화생성 서비스 테스팅 api 개발 후 diary 저장 메서드 및 엔티티 리펙토링

--- Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Controller/DiaryController.java
+++ Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Controller/DiaryController.java
@@ -1,16 +1,22 @@
 package MentalCare.ChatBot.domain.Diary.Controller;
 
 import MentalCare.ChatBot.domain.Diary.DTO.Request.DiaryRequest;
+import MentalCare.ChatBot.domain.Diary.DTO.Request.PromptRequest;
+import MentalCare.ChatBot.domain.Diary.DTO.Response.PromptResponse;
 import MentalCare.ChatBot.domain.Diary.Entity.Diary;
 import MentalCare.ChatBot.domain.Diary.Service.DiaryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.image.Image;
+import org.springframework.ai.image.ImageClient;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.Map;
 
 @Tag(name = "Diary", description = "일기 기능 API")
@@ -19,10 +25,9 @@ import java.util.Map;
 @RequestMapping("/api")
 public class DiaryController {
 
-    //아래 서비스는 인터페이스를 구현한 구현채까지 만들고 서비스 어노테이션까지 붙혀줘야 오류가 안남
     private final DiaryService diaryService;
-    private final OpenAiChatModel openAiChatModel;
-    //private final ImageModel imageModel;
+    private final ChatClient chatClient; //일기 요약 + 채팅 전용 객체
+    private final ImageClient imageClient; //일기를 통해 4칸 만화 생성 객체
 
     @Operation(summary = " 일기 저장 버튼 클릭 API ", description = " 일기 저장 버튼 클릭 시 일기 요약 + 4컷 카툰 제작 + 일기 저장 + 감정 분류 + 분류된 감정을 바탕으로 날씨 이름과 날씨 이모티콘 매칭 후 일기 본문과 만화를 반환하는 API")
     @PostMapping("/diary")
@@ -37,17 +42,7 @@ public class DiaryController {
         String weather = weatherMatch.get("weather");
         String weatherEmoji = weatherMatch.get("weatherEmoji");
 
-        // TODO : 엔티티에 set를 적용하고 있다. 추후 변동할것
-        // Diary 엔티티 생성 및 필드 설정
-        Diary diary = new Diary();
-        diary.setDiaryText(diaryText);
-        diary.setDiarySummary(diarySummary);
-        diary.setComicURL(comicURL);
-        diary.setDiaryEmotion(diaryEmotion);
-        diary.setWeather(weather);
-        diary.setWeatherEmoji(weatherEmoji);
-        diary.setDiaryDate(LocalDate.now()); // 오늘 날짜로 설정
-
+        Diary diary = new Diary(diaryText, diarySummary, comicURL, diaryEmotion, weather, weatherEmoji, LocalDate.now());
         diaryService.saveDiary(diary);
 
         return ResponseEntity.ok().body(Map.of(
@@ -56,8 +51,6 @@ public class DiaryController {
         ));
     }
 
-    //클라이언트가 선택한 날짜를 기반으로 한 일기 조회 메서드
-    //텍스트와 만화 URL만 반환
     @Operation(summary = " 일기 조회 API ", description = " 쿨라이언트 측에서 넘어오는 날자 값을 받아 당일날 일기 본문과 4컷 만화를 반환 ")
     @GetMapping("/diary")
     public ResponseEntity<?> DiaryShow(@RequestParam("date") String date){
@@ -69,34 +62,51 @@ public class DiaryController {
             return ResponseEntity.notFound().build(); // 일기가 없으면 404 Not Found 반환}
         }
 
-        // 응답 데이터 생성
         Map<String, Object> response = Map.of(
                 "diaryText", diary.getDiaryText(),
                 "comicURL", diary.getComicURL()
-        );
+        );// 응답 데이터 생성
 
         return ResponseEntity.ok().body(response);
     }
 
-    /*일기 요약 테스팅 용 API*/
-    //테스팅 성공 - 4줄요약으로 진행
-    //요청 답변 시간 - 2.04s
-    //Prompting 메시지 추가 요망
+
     // TODO : DTO을 받아서 ai 연산을 수행할 것인가? 아니면 String 값을 받아서 ai 연산을 수행할 것인가?
-    @PostMapping("/diary/sumarize/test")
-    public  Map<String, String> sumarizetest(@RequestBody String message){
-        Map<String, String> responses = new HashMap<>();
+    @PostMapping("/diary/sumarize")
+    public  String sumarizetest(@RequestBody String message){
+        //String openAiResponse = chatClient.call(message);
+        return chatClient.call(message);
+    }
 
-        String openAiResponse = openAiChatModel.call(message);
-        responses.put("openai(chatGPT) 응답", openAiResponse);
+    @PostMapping("/diary/generate")
+    public PromptResponse getImage(@RequestBody PromptRequest request) {
+        final String prompt = request.prompt();
 
-        return responses;
+        // input validation
+        if (StringUtils.isEmpty(prompt)) {
+            System.out.println("Prompt is required");
+            throw new IllegalArgumentException("Prompt is required");
+        }
 
-          /*아래 코드는 받은 DTO를 string으로 변환 한 후 사용 하는 것이다.*/
-//        Map<String, String> responses = new HashMap<>();
-//        String diaryRequestString = diaryRequest.toString();
-//
-//        String openAiResponse = openAiChatModel.call(diaryRequestString);
-//        responses.put("openai(chatGPT) 응답", openAiResponse);
+        System.out.println("Prompt: " + prompt);
+        System.out.println("Generating image...");
+
+        // image options
+        OpenAiImageOptions imageOptions = OpenAiImageOptions.builder()
+                .withQuality("standard")
+                .withHeight(1024)
+                .withN(1)
+                .withWidth(1792)
+                .build();
+
+        // image prompt
+        ImagePrompt imagePrompt = new ImagePrompt(prompt, imageOptions);
+
+        System.out.println("Calling image client...");
+        // class ep
+        Image img = imageClient.call(imagePrompt).getResult().getOutput();
+
+        System.out.println("Image generated successfully");
+        return new PromptResponse(img.getUrl());
     }
 }
