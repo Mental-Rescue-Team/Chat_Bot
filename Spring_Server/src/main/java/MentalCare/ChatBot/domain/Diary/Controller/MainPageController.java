b97a21628e6dde204c8dce7d22a3fdd5855dffdf
passionryu <rsy1225@naver.com>
2024-10-19 01:14:26 +0900

    refactor : 메인페이지 컨트롤러 주석 추가 리펙토링

--- Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Controller/MainPageController.java
+++ Spring_Server/src/main/java/MentalCare/ChatBot/domain/Diary/Controller/MainPageController.java
@@ -2,5 +2,8 @@ package MentalCare.ChatBot.domain.Diary.Controller;
 
 public class MainPageController {
     // TODO : 오늘의 날씨(이름) 불러오기 API 개발 -> 날씨 사진과 격려 메시지는 날씨 이름을 바탕으로 클라이언트 측에서 로드할 것임
+    /* todayWeather() 메서드 구현하기 */
+
     // TODO : 이번달의 날씨 이모티콘 전부 불러오기 API 개발
+    /* loadEmojis() 메서드 구현하기*/
 }
