88da928b72677e4f4e9d86402d5e61348ba8315e
passionryu <rsy1225@naver.com>
2024-12-02 12:33:18 +0900

    refcactor

--- Spring_Server/src/main/java/MentalCare/ChatBot/global/auth/JWt/JwtUtil.java
+++ Spring_Server/src/main/java/MentalCare/ChatBot/global/auth/JWt/JwtUtil.java
@@ -1,5 +1,7 @@
 package MentalCare.ChatBot.global.auth.JWt;
 
+import MentalCare.ChatBot.domain.Member.Entity.Member;
+import MentalCare.ChatBot.domain.Member.Repository.MemberRepository;
 import MentalCare.ChatBot.domain.Member.Role.Role;
 import MentalCare.ChatBot.global.Exception.ErrorCode;
 import MentalCare.ChatBot.global.Exception.MemberException;
@@ -17,6 +19,8 @@ import java.util.Date;
 @Component
 @RequiredArgsConstructor
 public class JwtUtil {
+
+    private final MemberRepository memberRepository;
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
@@ -83,7 +87,7 @@ public class JwtUtil {
     /**
      * jwt 토큰에서 유저 네임 추출
      * @param token JWT 토큰
-     * @return
+     * @return 사용자의 이름
      */
     public String extractUsername(String token) {
 
@@ -107,20 +111,51 @@ public class JwtUtil {
     }
 
     /**
+     * 요청에서 사용자 이름을 추출
+     * @param request 사용자 요청
+     * @return 요청하는 사용자의 이름
+     */
+    public String extractNameByRequest(HttpServletRequest request){
+
+        String userToken = extractTokenFromRequest(request);
+        validateToken_isTokenValid(userToken);
+
+        return extractUsername(userToken);
+    }
+
+    public Member extractMemberByRefreshToken(String refreshToken){
+
+        String username = extractUsername(refreshToken);
+
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new MemberException(ErrorCode.NOT_FOUND_MEMBER));
+
+        return member;
+    }
+
+    // Validation Method Below
+    // Validation Method Below
+    // Validation Method Below
+    // Validation Method Below
+    // Validation Method Below
+
+    /**
      * 토큰 만료 여부 확인
      * @param token JWT 토큰
-     * @return
+     * @return true & false
      */
     public boolean isTokenExpired(String token) {
+
         return extractClaims(token).getExpiration().before(new Date());
     }
 
     /**
      * 리프레시 토큰 유효성 검사
      * @param refreshToken 리프레시 토큰
-     * @return
+     * @return true & false
      */
     public boolean validateRefreshToken(String refreshToken) {
+
         try {
             Claims claims = extractClaims(refreshToken);
             return !isTokenExpired(refreshToken);
@@ -153,17 +188,5 @@ public class JwtUtil {
         }
     }
 
-    /**
-     * 요청에서 사용자 이름을 추출
-     * @param request 사용자 요청
-     * @return 요청하는 사용자의 이름
-     */
-    public String extractNameByRequest(HttpServletRequest request){
-
-        String userToken = extractTokenFromRequest(request);
-        validateToken_isTokenValid(userToken);
-
-        return extractUsername(userToken);
-    }
 
 }
