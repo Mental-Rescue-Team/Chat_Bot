8c41caf2d2d0c5e063e438323a401573e6a7d1ab
passionryu <rsy1225@naver.com>
2024-12-06 20:01:10 +0900

    refactor : 챗봇 도메인 리펙토링

--- Spring_Server/src/main/java/MentalCare/ChatBot/global/config/SecurityConfig.java
+++ Spring_Server/src/main/java/MentalCare/ChatBot/global/config/SecurityConfig.java
@@ -14,9 +14,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
